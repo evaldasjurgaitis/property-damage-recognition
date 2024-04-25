70c0299abbcbd7b249754bddcccc1e9ffcbf7c4f
Evaldas Jurgaitis <e.jurgaitis@ba.lt>
2024-04-25 07:34:56 +0300

    Init service

---
+++ src/main/java/ba/propertydamgerecognition/PropertyDamgeRecognitionApplication.java
@@ -0,0 +1,13 @@
+package ba.propertydamgerecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyDamgeRecognitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyDamgeRecognitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ba/propertydamgerecognition/PropertyDamgeRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.propertydamgerecognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyDamgeRecognitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
