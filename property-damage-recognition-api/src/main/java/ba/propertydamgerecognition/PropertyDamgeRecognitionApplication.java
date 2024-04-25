1d1bead9046dab1cb17a47fd9aa74f0496946a04
Evaldas Jurgaitis <e.jurgaitis@ba.lt>
2024-04-25 11:18:27 +0300

    Init ui

--- property-damage-recognition-api/src/main/java/ba/propertydamgerecognition/PropertyDamgeRecognitionApplication.java
+++ property-damage-recognition-api/src/main/java/ba/propertydamgerecognition/PropertyDamgeRecognitionApplication.java
@@ -2,8 +2,9 @@ package ba.propertydamgerecognition;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class PropertyDamgeRecognitionApplication {
 
 	public static void main(String[] args) {
